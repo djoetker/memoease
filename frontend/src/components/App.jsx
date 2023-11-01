aac6db3bd24f51447f1c93d7f16618956bdc9bc4
alisawiali <alisawiali70@gmail.com>
2023-11-01 19:01:09 +0100

    Update navigation and HomePage

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,18 @@
-import '..css/App.css';
-
+import "../css/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./layout/Navigation";
+import Container from "./HomePage/Container";
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Container />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
