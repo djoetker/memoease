4b641c16a5ece8a62295129abdfaf2678c02a910
Alsulayman <radwansulimann@gmail.com>
2023-11-07 11:43:10 +0100

    Home logged in Comp. und create set comp. erstellt

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,20 +2,18 @@ import "../css/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./layout/Navigation";
 
-
-
 import About from "./About/About";
 import Contact from "./Contact/Contact";
 import HomePage from "./HomePage/HomePage";
 import Login from "./Login_/Login";
 import Register from "./Register/Register";
-
+import { Box } from "./createSet/createSet";
+import HomeLoggedIn from "./homeLoggedIn/HomeLoggedIn";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-
         <Routes>
           <Route element={<Navigation />}>
             <Route path="/" element={<HomePage />} />
@@ -23,8 +21,9 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/create" element={<Box />} />
+            <Route path="/home" element={<HomeLoggedIn />} />
           </Route>
-
         </Routes>
       </BrowserRouter>
     </>
